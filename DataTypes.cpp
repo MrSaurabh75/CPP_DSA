fd3f1f9b2395e816aa8e229993e1c34cdd1d44b6
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-05-29 08:57:58 +0530

    C++ Basics

---
+++ DataTypes.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    cout<<"Size of int is "<<sizeof(a)<<endl;
+
+    float b = 3.14f;
+    cout<<"Size of float is "<<sizeof(b)<<endl;
+
+    char c = '$';
+    cout<<"Size of Char is "<<sizeof(c)<<endl;
+
+    double d = 44.484784774878748884848478;
+    cout<<"Size of double is "<<sizeof(d)<<endl;
+
+    bool e = 1;
+    cout<<"Size of boolean is "<<sizeof(e)<<endl;
+
+    short int f=23;
+    cout<<"Size of short is "<<sizeof(f)<<endl;
+
+    long int g;
+    cout<<"Size of long is "<<sizeof(g)<<endl;
+}
---
+++ inout.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter 1st amount :";
+    cin>>a;
+    cout<<"Enter 2nd amount :";
+    cin>>b;
+    int sum=a+b;
+    cout<<"Total amount is "<<sum<<endl;
+    return 0;
+}
