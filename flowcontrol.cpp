11949b677dd3246009789d02df6bd88d348f5a97
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-05-30 17:50:31 +0530

    Flow Control Statements in c++

---
+++ flowcontrol.cpp
@@ -0,0 +1,33 @@
+// Break Statements :-
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num;
+//     int i=2;
+//     cout<<"Enter a number :";
+//     cin>>num;
+//     for(i;i<=num;i++){
+//         if(num%i==0){
+//             break;
+//         }
+//     }
+//     if(num==i){
+//         printf("%d is a prime number",num);
+//     }
+//     else{
+//         printf("Not a prime number");
+//     }
+
+// }
+
+// Print the numbers from 1 to 100 except the numbers that are divisible by three
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=100;i++){
+//         if(i%3==0){
+//             continue;
+//         }
+//         cout<<i<<endl;
+//     }
+// }
