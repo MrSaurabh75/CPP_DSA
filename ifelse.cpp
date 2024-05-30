fcaefda23f03ed5a1014e0b93c1aceb2c5a9c78c
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-05-30 10:51:00 +0530

    If-Else Statement

---
+++ ifelse.cpp
@@ -0,0 +1,59 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int age;
+//     cout<<"Enter your age : "<<endl;
+//     cin>>age;
+//     if(age>18){
+//         cout<<"You can drive a car!";
+//     }
+//     else if(age==18){
+//         cout<<"Apply for licence!";
+//     }
+//     else{
+//         cout<<"You Cannot drive!";
+//     }
+// }
+
+// Find max from three given numbers
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cout<<"Enter a first number : ";
+//     cin>>a;
+//     cout<<"Enter a second number : ";
+//     cin>>b;
+//     cout<<"Enter a third number : ";
+//     cin>>c;
+
+//     if(a>b && a>c){
+//         cout<<a," is Maximum";
+//     }
+//     else if(b>a && b>c){
+//         cout<<b," is Maximum";
+//     }
+//     else{
+//         cout<<c," is Maximum";
+//     }
+//     return 0;
+// }
+
+// Find the given number is odd or even
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int num;
+//     cout<<"Enter a Number : ";
+//     cin>>num;
+//     if(num%2==0){
+//         cout<<"even";
+//     } 
+//     else{
+//         cout<<"odd";
+//     }
+
+//     return 0;
+// }
+
