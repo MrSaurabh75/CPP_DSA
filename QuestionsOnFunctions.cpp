032b34d3aa009b108cd4541169a707b4b7ad7289
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-02 09:28:00 +0530

    Arrays in c++

--- QuestionsOnFunctions.cpp
+++ QuestionsOnFunctions.cpp
@@ -15,43 +15,43 @@
 // }
 
 // Write a program to check the values are pythagorian or not
-#include<iostream>
-using namespace std;
-int checkIt(int a,int b,int c){
-    int x,y,z;
-    if(a>b && a>c){
-        x = a;
-        y = b;
-        z = c;
-    }
-    else if(b>a && b>c){
-        x = b;
-        y = a;
-        z = c;
-    }
-    else{
-        x = c;
-        y = a;
-        z = b;
-    }
-    int hypo = x*x;
-    int sides = (y*y)+(z*z);
-    if(hypo==sides){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-int main(){
-    int p,q,r;
-    cin>>p>>q>>r;
-    int result = checkIt(p,q,r);
-    if(result==1){
-        cout<<"Pythagorian!";
-    }
-    else{
-        cout<<"Not a Pythagorian!";
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int checkIt(int a,int b,int c){
+//     int x,y,z;
+//     if(a>b && a>c){
+//         x = a;
+//         y = b;
+//         z = c;
+//     }
+//     else if(b>a && b>c){
+//         x = b;
+//         y = a;
+//         z = c;
+//     }
+//     else{
+//         x = c;
+//         y = a;
+//         z = b;
+//     }
+//     int hypo = x*x;
+//     int sides = (y*y)+(z*z);
+//     if(hypo==sides){
+//         return 1;
+//     }
+//     else{
+//         return 0;
+//     }
+// }
+// int main(){
+//     int p,q,r;
+//     cin>>p>>q>>r;
+//     int result = checkIt(p,q,r);
+//     if(result==1){
+//         cout<<"Pythagorian!";
+//     }
+//     else{
+//         cout<<"Not a Pythagorian!";
+//     }
+//     return 0;
+// }
---
+++ arrays.cpp
@@ -0,0 +1,30 @@
+// Array:-
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int marks[5]={89,67,89,77,65};
+//     cout<<marks[2];
+// }
+// Write a program to take array from user and return the max & min element from array.
+// #include<iostream>
+// #include<climits>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<=n;i++){
+//         cin>>arr[i];
+//     }
+//     int max=INT_MIN;
+//     int min=INT_MAX;
+//     for(int i=0;i<=n;i++){
+//         if(arr[i]>max){
+//             max = arr[i];
+//         }
+//         if(arr[i]<min){
+//             min = arr[i];
+//         }
+//     }
+//     cout<<max<<" "<<min<<endl;
+// }
