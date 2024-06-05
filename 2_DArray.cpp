45d2f14054cedd601fc46042180ed670a4367884
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-05 09:14:21 +0530

    2D Array Searching element

--- 2_DArray.cpp
+++ 2_DArray.cpp
@@ -1,4 +1,24 @@
 // 2_D Arrays :
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n,m;
+//     cin>>n>>m;
+//     int arr[n][m];
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             cin>>arr[i][j];
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// Searching elemment in 2D array :
 #include<iostream>
 using namespace std;
 int main(){
@@ -10,10 +30,14 @@ int main(){
             cin>>arr[i][j];
         }
     }
+    int x;
+    cout<<"Enter key to search :";
+    cin>>x;
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
-            cout<<arr[i][j]<<" ";
+            if(arr[i][j]==x){
+                cout<<"("<<i<<","<<j<<")";
+            }
         }
-        cout<<endl;
     }
 }
