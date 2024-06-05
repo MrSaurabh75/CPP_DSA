5c2436e002fdacb13453e34412a6c94bdf504c3b
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-05 09:06:07 +0530

    2D Array

---
+++ arrayCh.cpp
@@ -0,0 +1,41 @@
+// Find max from array :
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int max=0;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]>max){
+//             max = arr[i];
+//         }
+//     }
+//     cout<<"Max :"<<max;
+// }
+
+// Find the sum of all subarrays :
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter size of array :";
+//     cin>>n;
+//     int arr[n];
+//     cout<<"Enter"<<n<<" elements in array :";
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int sum=0;
+//     for(int i=0;i<n;i++){
+//         sum=0;
+//         for(int j=i;j<n;j++){
+//             sum = sum+arr[j];
+//             cout<<sum<<endl;
+//         }
+//     }
+// }
+
