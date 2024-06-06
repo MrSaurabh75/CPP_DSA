3d01d3b4c2dc81c751264f518d8bec5fa4200c7e
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-06 22:12:12 +0530

    Character arrays

---
+++ charArray.cpp
@@ -0,0 +1,13 @@
+// Initialize an character array :
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char arr[100] = "saurabh chorge";
+//     int i = 0;
+//     while(arr[i]!='\0'){
+//         cout<<arr[i];
+//         i++;
+//     }
+// }
+
+// 
