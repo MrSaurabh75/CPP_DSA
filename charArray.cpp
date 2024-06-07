6d52d6ab4636dc8bd3e269d336fbfe9f08221a5a
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-07 09:49:45 +0530

    character  Array Challanges

--- charArray.cpp
+++ charArray.cpp
@@ -21,25 +21,53 @@
 // }
 
 // Check a given string is palindrome or not :
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     char arr[n+1];
+//     cin>>arr;
+//     bool check = 1;
+//     for(int i=0;i<n;i++){
+//         if(arr[i] != arr[n-1-i]){
+//             check = 0;
+//             break;
+//         }
+//     }    
+//     if(check == true){
+//         cout<<"Given string is palindrome!"<<endl;
+//     }
+//     else{
+//         cout<<"Given string is not palindrome!"<<endl;
+//     }
+//     return 0;
+// }
+
+// Find the word from a sentence which has max length :
 #include<iostream>
 using namespace std;
 int main(){
     int n;
     cin>>n;
+    cin.ignore();
     char arr[n+1];
-    cin>>arr;
-    bool check = 1;
-    for(int i=0;i<n;i++){
-        if(arr[i] != arr[n-1-i]){
-            check = 0;
+    cin.getline(arr,n);
+    int i=0;
+    int curLen = 0, maxLen = 0;
+    while(1){
+        if(arr[i]==' ' || arr[i]=='\0'){
+            if(curLen>maxLen){
+                maxLen=curLen;
+            }
+            curLen = 0;
+        }
+        else
+            curLen++;
+        if(arr[i]=='\0'){
             break;
         }
-    }    
-    if(check == true){
-        cout<<"Given string is palindrome!"<<endl;
+        i++;
     }
-    else{
-        cout<<"Given string is not palindrome!"<<endl;
-    }
-    return 0;
+    cout<<maxLen<<endl;
 }
