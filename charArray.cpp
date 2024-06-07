33934eb860edfea76eb96ff165b6e3be4e19f535
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-07 09:34:30 +0530

    2D Array Challanges

--- charArray.cpp
+++ charArray.cpp
@@ -10,4 +10,36 @@
 //     }
 // }
 
-// 
+// Take input from user :
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char arr[100];
+//     cin>>arr;
+//     cout<<arr<<endl;
+//     cout<<arr[3];
+// }
+
+// Check a given string is palindrome or not :
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+    bool check = 1;
+    for(int i=0;i<n;i++){
+        if(arr[i] != arr[n-1-i]){
+            check = 0;
+            break;
+        }
+    }    
+    if(check == true){
+        cout<<"Given string is palindrome!"<<endl;
+    }
+    else{
+        cout<<"Given string is not palindrome!"<<endl;
+    }
+    return 0;
+}
