b4644184b3c13c54e22ebd9e5dc540682ced8346
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-08 10:06:57 +0530

    Strings in CPP

---
+++ Strings.cpp
@@ -0,0 +1,13 @@
+// Strings :
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string str;
+    // cin>>str;
+    // cout<<str;
+
+    string s2;
+    getline(cin,s2);
+    cout<<s2<<endl;
+}
