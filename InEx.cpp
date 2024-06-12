a6a5275ba141fc25cb12996828392a58f0ac20cb
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-12 11:16:12 +0530

    Euclidean algorithm

--- InEx.cpp
+++ InEx.cpp
@@ -1,15 +1,33 @@
-// Inclusion-Exclusion :
+// // Inclusion-Exclusion :
+// #include<iostream>
+// using namespace std;
+// int div(int n,int a,int b){
+//     int c1 = n/a;
+//     int c2 = n/b;
+//     int c3 = n/(a*b);
+//     return c1+c2-c3;
+// }
+// int main(){
+//     int n,a,b;
+//     cin>>n>>a>>b;
+//     int result = div(n,a,b);
+//     cout<<result;
+// }
+
+// Euclid Algorithm tp find GCD :
 #include<iostream>
 using namespace std;
-int div(int n,int a,int b){
-    int c1 = n/a;
-    int c2 = n/b;
-    int c3 = n/(a*b);
-    return c1+c2-c3;
+int GCD(int a,int b){
+    while(b!=0){
+        int rem = a%b;
+        a=b;
+        b=rem;
+    }
+    return a; 
 }
 int main(){
-    int n,a,b;
-    cin>>n>>a>>b;
-    int result = div(n,a,b);
-    cout<<result;
+    int a,b;
+    cin>>a>>b;
+    int r = GCD(a,b);
+    cout<<r;
 }
