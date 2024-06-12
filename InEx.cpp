2ca8f3b1d635c9688895a148d201a7e76b9a2364
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-12 10:58:50 +0530

    Inclusive-Exclusive

---
+++ InEx.cpp
@@ -0,0 +1,15 @@
+// Inclusion-Exclusion :
+#include<iostream>
+using namespace std;
+int div(int n,int a,int b){
+    int c1 = n/a;
+    int c2 = n/b;
+    int c3 = n/(a*b);
+    return c1+c2-c3;
+}
+int main(){
+    int n,a,b;
+    cin>>n>>a>>b;
+    int result = div(n,a,b);
+    cout<<result;
+}
