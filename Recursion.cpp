ebfa3e330b624dad5a7cd2d1be416169c87ad543
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-13 21:38:28 +0530

    Recursion Basics

---
+++ Recursion.cpp
@@ -0,0 +1,44 @@
+// Recursion :
+// #include<iostream>
+// using namespace std;
+// int sum(int n){
+//     if(n==0){
+//         return 0;
+//     }
+//     return n+sum(n-1);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<sum(n);
+// }
+
+// Calculate the n to the power p :
+// #include<iostream>
+// using namespace std;
+// int Power(int n,int p){
+//     if(p==0){
+//         return 1;
+//     }
+//     return n*Power(n,p-1);
+// }
+// int main(){
+//     int n,p;
+//     cin>>n>>p;
+//     cout<<Power(n,p);
+// }
+
+// Calculate factorial of n using recursion :
+// #include<iostream>
+// using namespace std;
+// int fact(int n){
+//     if(n==0 || n==1){
+//         return 1;
+//     }
+//     return n*fact(n-1);
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     cout<<fact(n);
+// }
