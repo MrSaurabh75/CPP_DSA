1bdf9f574ac9a73fd0edb832917235e5eaf8e5ae
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-15 23:30:50 +0530

    Recursion

--- RQ.cpp
+++ RQ.cpp
@@ -1,25 +1,43 @@
 // Check if array is sorted or not!
-#include<iostream>
-using namespace std;
-bool check(int arr[],int n){
-    bool flag = true;
-    if(n==0 || n==1){
-        return true;
-    }
-    return arr[n]<arr[n+1];
-}
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    bool result = check(arr,n);
-    if(result==true){
-        cout<<"Array is sorted";
-    }
-    else{
-        cout<<"Array is not sorted";
-    }
-}
+// #include<iostream>
+// using namespace std;
+// bool check(int arr[],int n){
+//     if(n==1){
+//         return true;
+//     }
+//     bool restArray = check(arr+1,n-1);
+//     if(arr[0]<arr[1] && restArray){
+//         return true;
+//     }
+// }
+// int main(){
+//     int n = 5;
+//     int arr[n]={1,2,3,4,5,6};
+//     cout<<check(arr,n);
+// }
+
+// Print an numbers till n
+// #include<iostream>
+// using namespace std;
+// void dec(int n){
+//     if(n==0){
+//         return;
+//     }
+//     cout<<n<<" ";
+//     dec(n-1);
+// }
+// void inc(int n){
+//     if(n==0){
+//         return;
+//     }
+//     inc(n-1);
+//     cout<<n<<" ";
+// }
+// int main(){
+//     int n;
+//     cin>>n;
+//     dec(n);
+//     cout<<endl;
+//     inc(n);
+// }
+
