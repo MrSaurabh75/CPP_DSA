49d61424355d7303445e074dd152407a9d9901f1
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-18 22:11:20 +0530

    Inheritance

---
+++ Inheritance.cpp
@@ -0,0 +1,63 @@
+// Inheritance :
+#include<iostream>
+using namespace std;
+// Single Inheritance :
+// class A{
+//     public:
+//     void func(){
+//         cout<<"Single Inheritance";
+//     }
+// };
+// class B:public A{
+    
+// };
+// int main(){
+//     B b;
+//     b.func(); 
+// }
+
+// Multiple Inheritance :
+// class A{
+//     public:
+//     void func1(){
+//         cout<<"Multiple Inheritance A"<<endl;
+//     }
+// };
+// class B{
+//     public:
+//     void func2(){
+//         cout<<"Multiple Inheritance B"<<endl;
+//     }
+// };
+// class C : public A,public B{
+
+// };
+
+// int main(){
+//     C c;
+//     c.func1();
+//     c.func2();
+// }
+
+// Multilevel Inheritance :
+// class A{
+//     public:
+//     void func1(){
+//         cout<<"Multilevel Inheritance A"<<endl;
+//     }
+// };
+// class B : public A{
+//     public:
+//     void func2(){
+//         cout<<"Multilevel Inheritance B"<<endl;
+//     }
+// };
+// class C:public B{
+
+// };
+// int main(){
+//     C c;
+//     c.func1();
+//     c.func2();
+// } 
+
