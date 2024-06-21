675e7d4d256d676b03fec4242e8379d808b67a22
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-06-21 22:34:48 +0530

    Algorithms

--- AlgoRevision.cpp
+++ AlgoRevision.cpp
@@ -73,25 +73,61 @@ using namespace std;
 // }
 
 // Bubble Sort :
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int counter=1;
-    while(counter<n){
-        for(int i=0;i<n-counter;i++){
-            if(arr[i]>arr[i+1]){
-                int temp=arr[i];
-                arr[i]=arr[i+1];
-                arr[i+1]=temp;
-            }
-            counter+=1;
-        }
-    }
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-}
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int counter=1;
+//     while(counter<n){
+//         for(int i=0;i<n-counter;i++){
+//             if(arr[i]>arr[i+1]){
+//                 int temp=arr[i];
+//                 arr[i]=arr[i+1];
+//                 arr[i+1]=temp;
+//             }
+//             counter+=1;
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
+// sieve of eratosthenes :
+// void sieve(int n){
+//     int prime[100]={0};
+//     for(int i=0;i<n;i++){
+//         if(prime[i]==0){
+//             for(int j=i*i;j<=n;j+=i){
+//                 prime[j]=1;
+//             }
+//         }
+//     }
+//     for(int i=0;i<n;i++){
+//         if(prime[i]==0){
+//             cout<<i<<" ";
+//         }
+//     }
+// }
+// int main(){
+//     int n=10;
+//     sieve(n);
+// }
+
+// Euclidean Algorithm to find the GCD :
+// int GCD(int a,int b){
+//     while(b!=0){
+//         int rem = a%b;
+//         a=b;
+//         b=rem;
+//     }
+//     return a;
+// }
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     cout<<GCD(a,b)<<" is the GCD of "<<a<<" & "<<b;
+// }
