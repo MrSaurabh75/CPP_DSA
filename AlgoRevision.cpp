c7a5f64840d7fd57c7cb9322ffcc59a875a9a5ec
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-07-17 20:57:29 +0530

    Queue Oprations

--- AlgoRevision.cpp
+++ AlgoRevision.cpp
@@ -550,3 +550,62 @@ using namespace std;
 //         cout<<"Un-Balanced";
 //     }
 // }
+
+// Queue using array :
+class node{
+    public:
+    int data;
+    node* next;
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+class Queue{
+    node* front;
+    node* back; 
+    public:
+    Queue(){
+        front = NULL;
+        back = NULL;    
+    }
+    void enqueue(int x){
+        node* n = new node(x);
+        if(front==NULL){
+            front = n;
+            back = n;
+            return;
+        }
+        back->next=n;
+        back = n;
+    }
+    void dequeue(){
+        if(front==NULL){
+            cout<<"Queue is empty!";
+            return;
+        }
+        node* todelete = front;
+        front = front->next;
+        delete todelete;
+    }
+    int peek(){
+        if(front==NULL){
+            return -1;
+        }
+        return front->data;
+    }
+    bool empty(){
+        if(front==NULL){
+            return true;
+        }
+        return false;
+    }
+};
+int main(){
+    Queue q;
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    cout<<q.peek();
+}
